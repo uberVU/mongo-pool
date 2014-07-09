90840eeaef05e6f39630a58aa40da52456cc0fc6
Bogdan Ciobanu <bogdan.ciobanu@hootsuite.com>
2014-07-09 12:35:12 +0300

    Make version accessible from a single place #16
    Package version is parsed from the setup.py file, so to change the
    version, the single place where it has to be modified is in setup.py

--- mongopool/__init__.py
+++ mongopool/__init__.py
@@ -1,3 +1,15 @@
 from mongopool import MongoPool
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
 
 __all__ = ['mongopool']
+
+try:
+    _dist = get_distribution('mongopool')
+    if not __file__.startswith(os.path.join(_dist.location, 'mongopool')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
