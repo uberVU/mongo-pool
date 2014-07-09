025a9207dd9cc2a31310b94b0ce4856cf1826405
Bogdan Ciobanu <bogdan.ciobanu@hootsuite.com>
2014-07-09 13:29:55 +0300

    Write README.rst to render nicely on PyPi #16

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
-long_description = read('README.md')
+long_description = read('README.rst')
 
 setup(
     name='mongopool',
