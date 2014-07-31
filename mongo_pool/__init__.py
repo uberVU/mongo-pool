746f45d0bbccc73f4c3dda84d2e643a4d83c166b
Bogdan Ciobanu <bogdan.ciobanu@hootsuite.com>
2014-07-31 11:38:16 +0300

    Rename folder #25

--- mongo_pool/__init__.py
+++ mongo_pool/__init__.py
@@ -1,12 +1,13 @@
-from mongopool import MongoPool
+from mongo_pool import MongoPool
 from pkg_resources import get_distribution, DistributionNotFound
 import os.path
 
-__all__ = ['mongopool']
+__all__ = ['mongo_pool']
 
 try:
-    _dist = get_distribution('mongopool')
-    if not __file__.startswith(os.path.join(_dist.location, 'mongopool')):
+    _dist = get_distribution('mongo-pool')
+    import ipdb; ipdb.set_trace()
+    if not __file__.startswith(os.path.join(_dist.location, 'mongo_pool')):
         # not installed, but there is another version that *is*
         raise DistributionNotFound
 except DistributionNotFound:
